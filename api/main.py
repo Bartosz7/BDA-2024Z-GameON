5448c242d4016183815947319d84b4e98af3fd29
grzegorzZ1 <lukgrzes@gazeta.pl>
2024-11-16 23:22:58 +0100

    Added full API for f1

--- api/main.py
+++ api/main.py
@@ -1,11 +1,49 @@
 from fastapi import FastAPI
+import pandas as pd
 
 app = FastAPI()
 
-@app.get("/")
-def test():
-    return {"Elo": "Helo"}
+@app.get("/laps/{raceId}/{lap}")
+def laps(raceId, lap):
+    df = pd.read_csv("data/lap_times.csv")
+    df = df[(df["raceId"] == int(raceId)) & (df["lap"] == int(lap))]
+    out_dict = {}
+    try:
+        given_lap = {}
+        for _, driver in df.iterrows():
+            given_lap[driver.driverId] = {
+                "position": driver.position,
+                "milliseconds": driver.milliseconds
+            }
 
-@app.get("/test")
-def test_2():
-    return {"test": "test :)"}
+        out_dict["races"] = {
+            f"{raceId}": {
+                "laps": {
+                    f"{lap}": {
+                        "drivers": given_lap
+                    }
+                }
+            }
+        }
+    except:
+        out_dict = "Invalid data provided."
+
+    return out_dict
+
+@app.get("/races/{raceId}")
+def races(raceId):
+    df = pd.read_csv("data/races.csv")
+    df = df[(df["raceId"] == int(raceId))]
+
+    out_dict = {}
+    try:
+        out_dict["race"] = {
+            f"{raceId}": {
+                "name": df["name"].values[0],
+                "date": df["date"].values[0]
+            }
+        }
+    except:
+        out_dict = "Invalid data provided."
+
+    return out_dict
