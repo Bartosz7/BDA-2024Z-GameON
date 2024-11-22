1c59b5d46ffa1a9fe9445f39820bb1c3b46d9565
grzegorzZ1 <lukgrzes@gazeta.pl>
2024-11-22 22:53:24 +0100

    Added whole f1

--- dataflow_f1/dataflow_f1.py
+++ dataflow_f1/dataflow_f1.py
@@ -17,8 +17,9 @@ OUTPUT_TABLE = f'{PROJECT_ID}:post_data_test.posts'
 DATASET_ID = "f1"
 
 
-def process_lap_data(element):
-    races = element.get("races", {})
+def process_lap_data(json_data):
+    parsed_data = json.loads(json_data.decode('utf-8'))
+    races = parsed_data.get("races", {})
     for race_id, race_data in races.items():
         laps = race_data.get("laps", {})
         for lap, lap_data in laps.items():
@@ -28,10 +29,10 @@ def process_lap_data(element):
                     "lap": int(lap),
                     "driver": int(driver_id),
                     "position": driver_data["position"],
-                    "milliseconds": driver_data["milliseconds"],
                 }
 
 def calculate_statistics(element, position_history):
+    import numpy as np
     driver = element["driver"]
     lap = element["lap"]
     position = element["position"]
@@ -45,10 +46,10 @@ def calculate_statistics(element, position_history):
     min_position = np.min(position_history[driver])
     last_5_mean = np.mean(position_history[driver][-5:]) if len(position_history[driver]) >= 5 else mean_position
 
-    element["mean_position_up_to_lap"] = mean_position
-    element["std_position_up_to_lap"] = std_position
-    element["min_position_up_to_lap"] = min_position
-    element["last_5_laps_mean_position"] = last_5_mean
+    element["mean_position_up_to_lap"] = float(mean_position)
+    element["std_position_up_to_lap"] = float(std_position)
+    element["min_position_up_to_lap"] = float(min_position)
+    element["last_5_laps_mean_position"] = float(last_5_mean)
 
     return element
 
@@ -59,9 +60,9 @@ def run():
     gcp_options = options.view_as(GoogleCloudOptions)
     gcp_options.project = 'bda-gameon-demo'
     gcp_options.region = 'europe-west3'
-    gcp_options.job_name = 'f1-dataflow-pipeline-1'  # Unique job name
-    gcp_options.staging_location = 'gs://gameon-bucket-1/staging'  # Replace with your GCS bucket
-    gcp_options.temp_location = 'gs://gameon-bucket-1/temp'  # Replace with your GCS bucket
+    gcp_options.job_name = 'f1-dataflow-pipeline-3'
+    gcp_options.staging_location = 'gs://gameon-bucket-1/staging'
+    gcp_options.temp_location = 'gs://gameon-bucket-1/temp'
 
     with beam.Pipeline(options=options) as pipeline:
             position_history = beam.pvalue.AsSingleton(pipeline | "Create Empty History" >> beam.Create([{}]))
@@ -69,18 +70,17 @@ def run():
             (
                 pipeline
                 | "Read from Pub/Sub" >> beam.io.ReadFromPubSub(topic=TOPIC_ID)
-                | "Parse JSON" >> beam.Map(json.loads)
                 | "Extract Lap Data" >> beam.FlatMap(process_lap_data)
                 | "Calculate Statistics" >> beam.Map(calculate_statistics, position_history=position_history)
-                | "Write to BigQuery" >> beam.io.BigQueryDisposition.WriteToBigQuery(
-                    f'{PROJECT_ID}:{DATASET_ID}.laps',
+                | "Write to BigQuery" >> beam.io.WriteToBigQuery(
+                    f'{PROJECT_ID}:{DATASET_ID}.stream_laps',
                     schema=(
                         "lap:INTEGER, driver:INTEGER, position:INTEGER, "
                         "mean_position_up_to_lap:FLOAT, std_position_up_to_lap:FLOAT, "
-                        "min_position_up_to_lap:FLOAT, last_5_laps_mean_position:FLOAT, "
-                        "circuitId:INTEGER, age:INTEGER"
+                        "min_position_up_to_lap:FLOAT, last_5_laps_mean_position:FLOAT"
                     ),
                     write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
+                    create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED
                 )
             )
 
